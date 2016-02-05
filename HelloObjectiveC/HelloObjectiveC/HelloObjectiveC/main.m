eb29a5a2b5ff606619d2d74063cc88d146c401d6
hatlonely <hatlonely@gmail.com>
2016-02-05 13:46:24 +0800

    add nsnumber nesting

---
+++ HelloObjectiveC/HelloObjectiveC/HelloObjectiveC/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  HelloObjectiveC
+//
+//  Created by hatlonely on 16/2/5.
+//  Copyright © 2016年 hatlonely. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+void HelloNSNumber();
+void HelloNSString();
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        HelloNSNumber();
+        HelloNSString();
+    }
+    return 0;
+}
